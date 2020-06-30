e08a310924beb2510b466281d44e189ca2d7892b
Julio Merlos <juliocml92@gmail.com>
2020-06-30 15:11:05 -0500

    correccion de port a PORT

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json({extended:true})) //Alternativa a app.use(bodyParser.urlencoded({extended:true}));
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
